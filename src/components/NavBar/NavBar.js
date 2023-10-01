89d2498149d07d50e9ad461cc3f3587f22d725a1
Jeff <gb>
2023-10-01 22:06:21 +0200

    first commit

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -7,9 +7,9 @@ import { BsMessenger } from "react-icons/bs";
 import { CgMenuGridR } from "react-icons/cg";
 import { TbSmartHome } from "react-icons/tb";
 import { FiSearch } from "react-icons/fi";
-import { BsCaretDownFill } from "react-icons/bs";
 import { useSelector } from 'react-redux';
 import { IoNotifications } from 'react-icons/io5';
+import NavbarPopup from '../appPopup/NavbarPopup';
 
 
 
@@ -93,40 +93,7 @@ const NavBar = () => {
                                             <img className="w-10 h-10 rounded-full" src={userData.picture} alt="Profil" />
                                         </div>
                                         {nav &&
-                                            <div className="fixed top-14 right-2 rounded-lg shadow-lg border border-gray-200 flex ">
-                                                <div className="w-[600px] xl:w-[300px] bg-white p-4 rounded-lg shadow-lg transform transition-all ease-in-out duration-300 scale-100">
-                                                    <NavLink to={`/profile/${userData._id}`}>
-                                                        <div className=" flex flex-row justify-center h-12 w-[100%] xl:rounded-xl sm:rounded-full sm:w-16 xl:w-[100%] hover:bg-gray-400 p-2 xl:justify-start text-xl items-center space-x-2.5 hoverAnimation">
-
-                                                            <img className="w-10 h-10 rounded-full" src={userData.picture} alt="Profil" />
-                                                            <div className="xl:flex hidden">
-                                                                <h2 className="text-[16px]  font-semibold text-gray-900">{userData.pseudo}</h2>
-                                                            </div>
-                                                        </div>
-                                                    </NavLink>
-                                                    <div className='mt-4 '>
-                                                        <div className='flex justify-center h-12 w-[100%] xl:rounded-xl sm:rounded-full sm:w-16 xl:w-[100%] hover:bg-gray-400 p-2 xl:justify-start text-xl items-center space-x-2.5 hoverAnimation'>
-                                                            <TbSmartHome className='text-4xl text-blue-300' />
-                                                            <h4 className=' text-[16px] text-black hidden xl:inline'>
-                                                                Home
-                                                            </h4>
-                                                        </div>
-                                                        <div className='flex justify-center h-12 w-[100%] xl:rounded-xl sm:rounded-full sm:w-16 xl:w-[100%] hover:bg-gray-400 p-2 xl:justify-start text-xl items-center space-x-2.5 hoverAnimation'>
-                                                            <TbSmartHome className='text-4xl text-blue-300' />
-                                                            <h4 className=' text-[16px] text-black hidden xl:inline'>
-                                                                Home
-                                                            </h4>
-                                                        </div>
-                                                        <div className='flex justify-center h-12 w-[100%] xl:rounded-xl sm:rounded-full sm:w-16 xl:w-[100%] hover:bg-gray-400 p-2 xl:justify-start text-xl items-center space-x-2.5 hoverAnimation'>
-                                                            <TbSmartHome className='text-4xl text-blue-300' />
-                                                            <h4 className=' text-[16px] text-black hidden xl:inline'>
-                                                                Home
-                                                            </h4>
-                                                        </div>
-                                                    </div>
-
-                                                </div>
-                                            </div>
+                                            <NavbarPopup/>
                                         }
                                     </div>
                                     {/*<div className='xl:flex hidden justify-between items-center '  >
--- src/components/appPopup/NavbarPopup.js
+++ src/components/appPopup/NavbarPopup.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { TbSmartHome } from "react-icons/tb";
 
 function NavbarPopup() {
+
+    const userData = useSelector((state) => state.userReducer);
+
     return (
-        <div>
-            <p>
-                Bonjour
-            </p>
+        <div className="fixed top-14 right-2 rounded-lg shadow-lg border border-gray-200 flex ">
+            <div className="w-[600px] xl:w-[300px] bg-white p-4 rounded-lg shadow-lg transform transition-all ease-in-out duration-300 scale-100">
+                <NavLink to={`/profile/${userData._id}`}>
+                    <div className=" flex flex-row justify-center h-12 w-[100%] xl:rounded-xl sm:rounded-full sm:w-16 xl:w-[100%] hover:bg-gray-400 p-2 xl:justify-start text-xl items-center space-x-2.5 hoverAnimation">
+
+                        <img className="w-10 h-10 rounded-full" src={userData.picture} alt="Profil" />
+                        <div className="xl:flex hidden">
+                            <h2 className="text-[16px]  font-semibold text-gray-900">{userData.pseudo}</h2>
+                        </div>
+                    </div>
+                </NavLink>
+                <div className='mt-4 '>
+                    <div className='flex justify-center h-12 w-[100%] xl:rounded-xl sm:rounded-full sm:w-16 xl:w-[100%] hover:bg-gray-400 p-2 xl:justify-start text-xl items-center space-x-2.5 hoverAnimation'>
+                        <TbSmartHome className='text-4xl text-blue-300' />
+                        <h4 className=' text-[16px] text-black hidden xl:inline'>
+                            Home
+                        </h4>
+                    </div>
+                    <div className='flex justify-center h-12 w-[100%] xl:rounded-xl sm:rounded-full sm:w-16 xl:w-[100%] hover:bg-gray-400 p-2 xl:justify-start text-xl items-center space-x-2.5 hoverAnimation'>
+                        <TbSmartHome className='text-4xl text-blue-300' />
+                        <h4 className=' text-[16px] text-black hidden xl:inline'>
+                            Home
+                        </h4>
+                    </div>
+                    <div className='flex justify-center h-12 w-[100%] xl:rounded-xl sm:rounded-full sm:w-16 xl:w-[100%] hover:bg-gray-400 p-2 xl:justify-start text-xl items-center space-x-2.5 hoverAnimation'>
+                        <TbSmartHome className='text-4xl text-blue-300' />
+                        <h4 className=' text-[16px] text-black hidden xl:inline'>
+                            Home
+                        </h4>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
