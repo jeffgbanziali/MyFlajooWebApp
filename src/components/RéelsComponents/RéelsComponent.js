8f598a2cb99fc589b3ea78459abe1a53eec4140a
Jeff <gb>
2023-10-17 20:51:20 +0200

    my commit

--- src/components/RéelsComponents/RéelsComponent.js
+++ src/components/RéelsComponents/RéelsComponent.js
@@ -14,9 +14,11 @@ import LikeRéelsButton from "./LikeRéelsButton";
 const RéelsComponent = () => {
   const [loadPosts, setLoadPosts] = useState(true);
   const [loading, setLoading] = useState(true);
+  const [reelsComment, setReelsComment] = useState(false);
   const dispatch = useDispatch();
   const reelsData = useSelector((state) => state.videoReelsReducer);
   const usersData = useSelector((state) => state.usersReducer);
+  const userData = useSelector((state) => state.userReducer);
 
   useEffect(() => {
     if (loadPosts) {
@@ -154,7 +156,10 @@ const RéelsComponent = () => {
                           {reels.likers.length}
                         </p>
                       </div>
-                      <div className="flex flex-col justify-center items-center mt-6 ">
+                      <div
+                        onClick={() => setReelsComment(true)}
+                        className="flex flex-col justify-center items-center mt-6 "
+                      >
                         <div className="flex flex-col w-12 h-12  cursor-pointer justify-center items-center rounded-full">
                           <BiMessageRounded className="text-white" size={30} />
                         </div>
@@ -162,6 +167,67 @@ const RéelsComponent = () => {
                           {reels.comments.length}
                         </p>
                       </div>
+                      {reelsComment && (
+                        <>
+                          <div className=" absolute z-40 flex-col p-4 top-8 bg-blue-600 rounded-3xl right-8 w-[25%] h-[60%]  flex ">
+                            <div className="flex justify-between items-center flex-row">
+                              <h4>Commentaires</h4>
+                              <span
+                                className="text-gray-200 flex mr-2  justify-end text-xl cursor-pointer"
+                                onClick={() => setReelsComment(false)}
+                              >
+                                &#10005;
+                              </span>
+                            </div>
+                            <div className=" mt-2 w-full h-full overflow-y-scroll bg-green-500">
+                              <div className="flex p-2 bg-red-700  ">
+                                <img
+                                  src={
+                                    !isEmpty(usersData[0]) &&
+                                    usersData
+                                      .map((user) => {
+                                        if (
+                                          user._id ===
+                                          reels.comments.commenterId
+                                        )
+                                          return user.picture;
+                                        else return null;
+                                      })
+                                      .join("")
+                                  }
+                                  className="w-8 h-8 items-center hover:opacity-75 cursor-pointer flex object-cover border shadow-md  border-red-500 rounded-14 ml-18 overflow-hidden rounded-full "
+                                  alt="commenter-pic"
+                                />
+                                <div>
+                                  <div className="flex flex-col ml-3 text-justify border-gray-200   ">
+                                    <h4 className="text-black  text-[14px] font-semibold">
+                                      rehrierlebrierjr
+                                    </h4>
+                                    <p className="text-gray-800 ml-1  text-[12px]">
+                                      flfhienir
+                                    </p>
+                                  </div>
+                                  <div className="flex flex-row space-x-2 ml-3  items-center mt-2">
+                                    <div className="cursor-pointer">
+                                      <h4 className="text-gray-400 hover:text-red-500 text-[10px] font-semibold">
+                                        Like
+                                      </h4>
+                                    </div>
+                                    <div className="cursor-pointer">
+                                      <h4 className="text-gray-400 hover:text-red-500 text-[10px] font-semibold">
+                                        Respond
+                                      </h4>
+                                    </div>
+                                    <h4 className="text-gray-700 text-[10px] font-semibold">
+                                      fdghufe
+                                    </h4>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </>
+                      )}
                       <div className="flex flex-col justify-center items-center mt-6 ">
                         <div className="flex flex-col w-12 h-12   cursor-pointer justify-center items-center rounded-full">
                           <IoSendOutline className="text-white" size={30} />
